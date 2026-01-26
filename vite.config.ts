19474fc3aadc4a8255510ea6d0b11ab17ec5cbf8
ThaRealJozef <yjrhider@gmail.com>
2026-01-27 01:08:40 +0700

    configured Vite base path to root and add ignore Vercel deployment files rule to gitignore

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/portfolio/' : '/',
+  base: '/',
   server: {
     host: true,
   },
